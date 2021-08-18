a03837e4af9ef262849720bc09f977ed4e509502
r197972x <85687520+r197972x@users.noreply.github.com>
2021-08-18 09:13:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import pandas
 import pickle
 
 
@@ -24,19 +25,22 @@ def main():
         
        
 
-        input_variables = pd.DataFrame([[Pregnancies,Glucose,BloodPressure,SkinThickness
+        input_variables = pandas.DataFrame([[Pregnancies,Glucose,BloodPressure,SkinThickness
         ,Insulin,BMI,DiabetesPedigreeFunction,Age]],columns=['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin',
        'BMI', 'DiabetesPedigreeFunction', 'Age'
        ])
 
         prediction = model.predict(input_variables)
+        prob = model.predict_proba(input_variables)[0]
+        factor = prob[0] *100
+        possibility_factor = str(round(factor, 1)) +'%'
        
         if prediction == 0:
             result = 'NO'
         else:
             result = 'YES'
 
-        return render_template('main.html',result=prediction)
+        return render_template('main.html',result=result,possibility_factor=possibility_factor )
 
 
 if __name__ == '__main__':
